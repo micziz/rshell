eb394eed8361bf9971ed0aae96e39b930ddd6743
micziz <miczicontent@gmail.com>
2022-04-08 08:52:04 +0200

    Compleate rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,49 @@
-fn main(){
+// rShell
+// rShell is a simple shell written in Rust.
+// it fully supports the POSIX shell commands.
+// it's also a scripting language.
+
+
+// Import required libraries
+use std::io::stdin;
+use std::io::Write;
+use std::process::Command;
+
+// Main function
+fn main() {
     loop {
-        print!("> ");
-        stdout().flush();
+        // Print the prompt
+        print!("rShell> ");
+        // Flush the output buffer
+        std::io::stdout().flush();
 
+        // Get user input
         let mut input = String::new();
-        stdin().read_line(&mut input).unwrap();
+        // Read user input
+        stdin().read_line(&mut input)
+            .ok()
+            .expect("Failed to read line");
+        
+        // Remove the newline character
+        // Split the input into parts
+        let mut parts = input.trim().split_whitespace();
+        // Get the command
+        let command = parts.next().unwrap();
+        // Get the arguments
+        let args = parts;
 
-        // must be peekable so we know when we are on the last command
-        let mut commands = input.trim().split(" | ").peekable();
-        let mut previous_command = None;
 
-        while let Some(command) = commands.next()  {
+    
+    
+        // Run the command in the shell
+        let mut child = Command::new(command)
+            .args(args)
+            .spawn()
+            .expect("Failed to run command");
 
-            let mut parts = command.trim().split_whitespace();
-            let command = parts.next().unwrap();
-            let args = parts;
-
-            match command {
-                "cd" => {
-                    let new_dir = args.peekable().peek()
-                        .map_or("/", |x| *x);
-                    let root = Path::new(new_dir);
-                    if let Err(e) = env::set_current_dir(&root) {
-                        eprintln!("{}", e);
-                    }
-
-                    previous_command = None;
-                },
-                "exit" => return,
-                command => {
-                    let stdin = previous_command
-                        .map_or(
-                            Stdio::inherit(),
-                            |output: Child| Stdio::from(output.stdout.unwrap())
-                        );
-
-                    let stdout = if commands.peek().is_some() {
-                        // there is another command piped behind this one
-                        // prepare to send output to the next command
-                        Stdio::piped()
-                    } else {
-                        // there are no more commands piped behind this one
-                        // send output to shell stdout
-                        Stdio::inherit()
-                    };
-
-                    let output = Command::new(command)
-                        .args(args)
-                        .stdin(stdin)
-                        .stdout(stdout)
-                        .spawn();
-
-                    match output {
-                        Ok(output) => { previous_command = Some(output); },
-                        Err(e) => {
-                            previous_command = None;
-                            eprintln!("{}", e);
-                        },
-                    };
-                }
-            }
-        }
-
-        if let Some(mut final_command) = previous_command {
-            // block until the final command has finished
-            final_command.wait();
-        }
-
+        // do not acccept other commands until the current command is finished
+        child.wait()
+            .expect("Failed to wait for command");
     }
+
 }
